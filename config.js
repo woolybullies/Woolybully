2e62ad5e8ad4bd243038bdc43a31e01f59f30080
Stephen Malito <stevemalito@gmail.com>
2019-06-22 10:33:22 -0500

    intalled modules, server.js, config.js, .env, and updated package.json

---
+++ config.js
@@ -0,0 +1,13 @@
+let config = {
+    local: {
+        mysql:{
+            url: process.env.DB_URL
+        },
+        apiKeys:{}
+    },
+    prod: {
+        mysql:{},
+        apiKeys:{}
+    }
+};
+module.exports = config[process.env.APP_ENV || 'local'];
