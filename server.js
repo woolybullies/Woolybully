3942de772df5b06200210a067e4ccf17d063ba30
Stephen Malito <stevemalito@gmail.com>
2019-06-22 10:45:39 -0500

    updated server.js to es6 variants

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 9000;
-var config = require('./config');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 9000;
+const config = require('./config');
 
 app.use(express.static(__dirname + '/views')); // you should change this to be wherever your html files are
 app.use(express.urlencoded({extended: true}));
