9f94805518d4b25c8385c5e6990ec94f6a1f7d6d
Phil Dugliss <phillipdugliss@gmail.com>
2019-06-24 19:16:40 -0500

    ORM and Controllers mini update
    -First route added
    -arr var changed on ORM file

--- controllers/woolyController.js
+++ controllers/woolyController.js
@@ -4,3 +4,10 @@ var router = express.Router();
 
 var wooly = require("../models/wooly.js");
 
+router.get("/" , function(request, response){
+    wooly.all(function(data){
+
+        //connect to front end here
+        console.log(data, request, response);
+    })
+})
