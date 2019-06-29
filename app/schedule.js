4411975461288b9e497ba1ee2fe5317f5a077e6d
Stephen Malito <stevemalito@gmail.com>
2019-06-29 16:37:57 -0500

    nested cron

--- app/schedule.js
+++ app/schedule.js
@@ -5,25 +5,58 @@ var goal_config = require('../models/goal_config')
 
 
 
+//cron schedule to query for what users need to be reminded
+cron.schedule('*/30 * * * * *', () => {
+  var d = new Date();
+  var n = d.getHours();
+  var cTime = `* * ${n} * * *`
+  console.log("running", n)
 
-// cron.schedule('*/2 * * * * *', () => {
-//   var d = new Date();
-// var n = d.getHours();
-//   console.log("running", n )
-//   goal_config.allGoals(function (result){
-//     console.log(result)
-//   })
+  goal_config.allGoals(function (result) {
+    console.log(result)
+    
+    
+      
+        if (time = cTime) {
 
-  
-// });
+          for (i = 0; i < result.length; i++) {
+            let name = result[i].name
+            
+            let phone = result[i].phone
+            
+            console.log(name)
+    
+
+          cron.schedule(time, () =>  {
+          const accountSid = process.env.accountSid;
+          const authToken = process.env.authToken;
+          const client = require('twilio')(accountSid, authToken);
+      
+
+          let msg = `Guess who didnt ${name} today`
+          console.log(msg);
+          client.messages
+            .create({
+              body: `${msg}`,
+              from: '+13125846791',
+              to: `+1${phone}`
+            })
+            .then(message => console.log(message.sid));
+
+          });  
+        } 
+      }
+
+    });
+});
 
 
 
 
 
 // function push() {
-//   const accountSid = 'AC5e07a56b471f643ce750033518d5bed1';
-//   const authToken = '30c27ebf3d2276a0276b576905ca2410';
+//   const accountSid = process.env.accountSid;
+//   const authToken = process.env.authToken;
 //   const client = require('twilio')(accountSid, authToken);
 //   for (i = 0; i < userObject.length; i++) {
 //     let name = userObject[i].name
