61a9f10100423c3c157ccc60a8a8a9734f1c66bb
Stephen Malito <stevemalito@gmail.com>
2019-07-03 15:33:10 -0500

    updated creds

--- app/schedule.js
+++ app/schedule.js
@@ -1,73 +1,73 @@
 var cron = require('node-cron');
 var goal_config = require('../models/goal_config')
 
+var d = new Date();
+
+var h = d.getHours();
+var m = ("0" + d.getMinutes()).slice(-2)
+
+var dd = ("0" + d.getDate()).slice(-2)
+var mm = ("0" + (d.getMonth() + 1)).slice(-2)
+var yyyy = d.getFullYear();
+
+var curDate = `${yyyy}-${mm}-${dd}`;
+//format to update time stamp on SQL
+var upd = { last_fired: `${curDate}` }
+var cTime = `* ${m} ${h} * * *`
+
 
 //cron schedule to query for what users need to be reminded
 cron.schedule('*/15 * * * * *', () => {
   //set the current date for posting to sql  
-  var d = new Date();
 
-  var h = d.getHours();
-  var m = ("0" + d.getMinutes()).slice(-2)
 
-  var dd = ("0" + d.getDate()).slice(-2)
-  var mm = ("0" + (d.getMonth() + 1)).slice(-2)
-  var yyyy = d.getFullYear();
-
-  var curDate = `${yyyy}-${mm}-${dd}`;
-  //format to update time stamp on SQL
-  var upd = { last_fired: `${curDate}` }
-  var cTime = `* ${m} ${h} * * *`
-  
   console.log(`
-
-  running ${h} ${curDate} ${cTime}
+running ${h} ${curDate} ${cTime}
   `)
-//  queryAlerts(cTime, upd)
-goal_config.allGoals(function (result) {
-  // console.log(result)
-  
-  //loop api response for data formatting to twilio API
-  for (i = 0; i < result.length; i++) {
-    var time = result[i].daily_occurance
-    var phone = result[i].phone
-    var id = result[i].id
-    var goalName = result[i].name
-    // console.log(time)
-    console.log(`${phone} - ${id} - ${goalName} - ${time}`)
-  }
+  queryAlerts(cTime, upd)
+});
+
+//query mysql for alerts for today
+
+function queryAlerts(cTime, upd) {
+  goal_config.allGoals(function (result) {
+    // console.log(result)
+
+    //loop api response for data formatting to twilio API
+    for (i = 0; i < result.length; i++) {
+      var time = result[i].daily_occurance
+      var phone = result[i].phone
+      var id = result[i].id
+      var goalName = result[i].name
+      // console.log(time)
+      console.log(`${phone} - ${id} - ${goalName} - ${time}`)
+    }
     //if the time the user needs to be reminded matches the current time
     if (time === cTime) {
       console.log("match", id)
-     
-      // console.log(`${phone} - ${id} - ${goalName}`)
+
       sendText(goalName)
       updateTable(upd, id)
-    
-    } else{
-     console.log("nothing to send")
+
+    } else {
+      console.log("nothing to send")
     }
-  
-
-
-});
-});
 
 
 
-function queryAlerts (cTime, upd){
-  //query sql for any goals that need reminding
-  
+  });
 
-  
-  
-  }
+}
 
 
 
 
-function sendText (goalName){
-  // cron.schedule(time, () => {
+
+
+
+
+function sendText(goalName) {
+
   const accountSid = process.env.accountSid;
   const authToken = process.env.authToken;
   const client = require('twilio')(accountSid, authToken);
@@ -76,25 +76,27 @@ function sendText (goalName){
   let msg = `Guess you're not gonna ${goalName} today`
 
   console.log(msg);
-  // client.messages
-  //   .create({
-  //     body: `${msg}`,
-  //     from: '+13125846791',
-  //     to: `+1${phone}`
-  //   })
-  //   .then(message => console.log(message.sid));
 
-  
-    // console.log(data)
-  // });
+  //send text via twillio  
+  client.messages
+    .create({
+      body: `${msg}`,
+      from: '+13125846791',
+      to: `+1${phone}`
+    })
+    .then(message => console.log(message.sid));
+
+
+  console.log(data)
+
 
 }
 
-function updateTable (upd, id){
+function updateTable(upd, id) {
   goal_config.updateOne(upd, id, function (data) {
     console.log(`Updated ${id}: ${upd}`)
   });
-  
+
 
 
 }
--- app/send_sms.js
+++ app/send_sms.js
@@ -1,14 +1,49 @@
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Your Account Sid and Auth Token from twilio.com/console
-// DANGER! This is insecure. See http://twil.io/secure
-const accountSid = 'AC5e07a56b471f643ce750033518d5bed1';
-const authToken = '30c27ebf3d2276a0276b576905ca2410';
-const client = require('twilio')(accountSid, authToken);
 
-client.messages
-  .create({
-     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: '+13125846791',
-     to: '+17084769340'
-   })
-  .then(message => console.log(message.sid));
+/*
+*	Code snippet for posting tweets to your own twitter account from node.js.
+*	You must first create an app through twitter, grab the apps key/secret,
+*	and generate your access token/secret (should be same page that you get the 
+*	app key/secret).
+* 	Uses oauth package found below:
+*		https://github.com/ciaranj/node-oauth
+*		npm install oauth
+*	For additional usage beyond status updates, refer to twitter api
+*		https://dev.twitter.com/docs/api/1.1
+*/
+
+var OAuth = require('oauth');
+
+var twitter_application_consumer_key = '';  // API Key
+var twitter_application_secret = '';  // API Secret
+var twitter_user_access_token = '';  // Access Token
+var twitter_user_secret = '';  // Access Token Secret
+
+var oauth = new OAuth.OAuth(
+	'https://api.twitter.com/oauth/request_token',
+	'https://api.twitter.com/oauth/access_token',
+	twitter_application_consumer_key,
+	twitter_application_secret,
+	'1.0A',
+	null,
+	'HMAC-SHA1'
+);
+
+var status = '';  // This is the tweet (ie status)
+
+var postBody = {
+	'status': status
+};
+
+// console.log('Ready to Tweet article:\n\t', postBody.status);
+oauth.post('https://api.twitter.com/1.1/statuses/update.json',
+	twitter_user_access_token,  // oauth_token (user access token)
+    twitter_user_secret,  // oauth_secret (user secret)
+    postBody,  // post body
+    '',  // post content type ?
+	function(err, data, res) {
+		if (err) {
+			console.log(err);
+		} else {
+			// console.log(data);
+		}
+	});
