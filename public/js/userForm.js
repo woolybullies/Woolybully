bcf48ea5e953b90518a0e69816255d3cf7c937d4
elletiburon <elleharrer@gmail.com>
2019-07-05 13:35:14 -0500

    about page to user page flow

--- public/js/userForm.js
+++ public/js/userForm.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
 
-    $("#submit").click(function(event){
+    $("#submit").click(function (event) {
         event.preventDefault()
 
         console.log("Clicked")
@@ -10,17 +10,18 @@ $(document).ready(function(){
         let email = $("#formEmail").val();
         let phone = $("#formPhoneNumber").val();
 
-            $.ajax({
-                type: "POST",
-                url: "/api/users",
-                data: { "name": name, "email": email, "phone": phone},
-                success: function (data, textStatus, jQxhr) {
-                    console.log(data);
-                },
-                error: function (jqXhr, textStatus, errorThrown) {
-                    console.log(errorThrown);
-                },
-                dataType: "JSON"
-            })
+        $.ajax({
+            type: "POST",
+            url: "/api/users",
+            data: { "name": name, "email": email, "phone": phone },
+            success: function (data, textStatus, jQxhr) {
+                console.log(data);
+            },
+            error: function (jqXhr, textStatus, errorThrown) {
+                console.log(errorThrown);
+            },
+            dataType: "JSON"
+        })
+        
     });
 });
