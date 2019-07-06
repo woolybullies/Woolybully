cef587224147b6b6dbdfcadf80c41e4255368d0d
Stephen Malito <stevemalito@gmail.com>
2019-07-05 20:13:13 -0500

    removed helper config for mysql connection so process.env is used and credentials are hidden

--- config/config.js
+++ config/config.js
@@ -7,14 +7,7 @@ if (process.env.DB_URL) {
     // Database is JawsDB on Heroku
     connection = mysql.createConnection(process.env.DB_URL);
 } else {
-    // Database is local
-    connection = mysql.createConnection({
-        port: 3306,
-        host: 'v02yrnuhptcod7dk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: 'qvi25n47mbm3c8da',
-        password: 'ffx51olsemcudjmv',
-        database: 'w5rf5aqxit0qej3v'
-    })
+    console.log(`Cannot to connect to mysql`)
 };
 
 
