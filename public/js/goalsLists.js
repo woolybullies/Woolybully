0ddf93248f722ccf61d737015cb4a2f1cdd9b3d0
elletiburon <elleharrer@gmail.com>
2019-07-06 10:31:14 -0500

    try this for ID also background color added because yolo

--- public/js/goalsLists.js
+++ public/js/goalsLists.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
   $('.collapsible').collapsible();
 
-  
+  $("#goal_delete").click(function (event) {
+    event.preventDefault();
+
+    var checked = jQuery('input:checkbox:checked').map(function () {
+        console.log($(this).attr("id"));
+        $.ajax({
+            type: "DELETE",
+            url: "/api/goals/"+$(this).attr("id"),
+            
+            success: function (data, textStatus, jQxhr) {
+                console.log(data);
+            },
+            error: function (jqXhr, textStatus, errorThrown) {
+                console.log(errorThrown);
+            },
+            dataType: "JSON"
+        })
+
+        return this.value;
+    }).get();
+    jQuery('input:checkbox:checked').parents("tr").remove();
+
+});
+
 });
--- public/js/view.js
+++ public/js/view.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
 
         console.log("Clicked")
 
+        
         let name = $("#goal").val();
         let time = $("#goalTime").val();
         let daily_occurance = `* * ${time} * * *`;
@@ -21,7 +22,7 @@ $(document).ready(function(){
             data: { "name": name, "daily_occurance": daily_occurance },
 
             success: function (data, textStatus, jQxhr) {
-                let checkbox = '<input type = "checkbox" id = "' + data.insertId + '">'
+                // let checkbox = '<input type = "checkbox" id = "' + data.insertId + '">'
 
                 console.log(data);
 
@@ -29,7 +30,7 @@ $(document).ready(function(){
                     $("<td>").text(data.insertId),
                     $("<td>").text(name),
                     $("<td>").text(daily_occurance),
-                    $("<td>").html(checkbox),
+                    
                 );
                 $("#goal-table > tbody").append(newRow);
             },
@@ -40,30 +41,7 @@ $(document).ready(function(){
         })
     });
 
-    $("#goal_delete").click(function (event) {
-        event.preventDefault();
-
-        var checked = jQuery('input:checkbox:checked').map(function () {
-            console.log($(this).attr("id"));
-            $.ajax({
-                type: "DELETE",
-                url: "/api/goals/"+$(this).attr("id"),
-                
-                success: function (data, textStatus, jQxhr) {
-                    console.log(data);
-                },
-                error: function (jqXhr, textStatus, errorThrown) {
-                    console.log(errorThrown);
-                },
-                dataType: "JSON"
-            })
-
-            return this.value;
-        }).get();
-        jQuery('input:checkbox:checked').parents("tr").remove();
-
-    });
-
+    
 
 
   });
