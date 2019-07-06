334e03da9e473700bbaa7a2181e01cd29b6b686c
Stephen Malito <stevemalito@gmail.com>
2019-07-05 20:18:22 -0500

    JAWSDB_URL

--- config/config.js
+++ config/config.js
@@ -3,9 +3,9 @@
 var mysql = require('mysql');
 
 var connection;
-if (process.env.DB_URL) {
+if (process.env.JAWSDB_URL) {
     // Database is JawsDB on Heroku
-    connection = mysql.createConnection(process.env.DB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     console.log(`Cannot to connect to mysql`)
 };
