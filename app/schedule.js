454c7ced7b5f4a7029c42ad60de6abb5d3f482b4
Stephen Malito <stevemalito@gmail.com>
2019-07-05 23:26:03 -0500

    added comments and formatting to schedule

--- app/schedule.js
+++ app/schedule.js
@@ -8,26 +8,31 @@ const client = require('twilio')(accountSid, authToken);
 
 
 
-//cron schedule to query for what users need to be reminded
+/*
+Cron schedule to establish time stamps and execute functions
+to query the databse for texts and calls that need to be made
+*/
 cron.schedule('*/15 * * * * *', () => {
-  //set the current date for posting to sql  
+
+//Generate Current Time
 var d = new Date();
 var h = d.getHours();
-var cH = h + 2;
 var m = ("0" + d.getMinutes()).slice(-2);
+var textTime = `* ${m} ${h} * * *`
 
-
+//Generate Date in needed format
 var dd = ("0" + d.getDate()).slice(-2)
 var mm = ("0" + (d.getMonth() + 1)).slice(-2)
 var yyyy = d.getFullYear();
-
 var curDate = `${yyyy}-${mm}-${dd}`;
+
+//Modify current time to trigger calls 2 hours prior 
+var cH = h + 2;
+var callTime = `* ${m} ${cH} * * *`
+
 //format to update time stamp on SQL
 var texted = { last_fired: `${curDate}` }
 var called = { last_called: `${curDate}` }
-var textTime = `* ${m} ${h} * * *`
-var callTime = `* ${m} ${cH} * * *`
-
 
   console.log(`
 running ${h}:${m} | ${curDate} | ${textTime} | ${callTime}
@@ -36,7 +41,7 @@ running ${h}:${m} | ${curDate} | ${textTime} | ${callTime}
   queryCalls(callTime, called)
 });
 
-//query mysql for alerts for today
+//query mysql for Text Alerts
 
 function queryAlerts(textTime, texted) {
   goal_config.allGoals(function (result) {
@@ -79,7 +84,7 @@ function queryCalls(callTime, called) {
       console.log(`Calls ${phone} - ${id} - ${goalName} - ${time}`)
     }
 
-    //if the time the user needs to be reminded matches the current time
+    //if the time matches 2 hours prior to the current time, send the call
     if (time === callTime) {
       console.log("call match", id)
       callUser(phone, id)
@@ -91,9 +96,6 @@ function queryCalls(callTime, called) {
 }
 
 function sendText(goalName, phone) {
-
-
-
 
   let msg = `Guess you're not gonna ${goalName} today`
 
@@ -108,12 +110,22 @@ function sendText(goalName, phone) {
     })
     .then(message => console.log(message.sid));
 
+}
+//Call User via Twilio 
+function callUser(phone, id) {
 
-
-
-
+  client.calls
+    .create({
+      //url must be PUBLIC address, in order for Twilio text-2-speech module to function
+      //an xml page must be generated with the text to be read off, and it must be public
+      url: `https://agile-wildwood-70962.herokuapp.com/api/twiml/${id}`,
+      to: `+1${phone}`,
+      from: `+1${twilioPhone}`
+    })
+    .then(call => console.log(call.sid));
 }
 
+//update the table both for texts and calls so that duplicates are not made in the same day
 function updateTable(update, id) {
   goal_config.updateOne(update, id, function (data) {
     console.log(`Updated ${id}: ${update}`)
@@ -124,13 +136,3 @@ function updateTable(update, id) {
 
 }
 
-function callUser(phone, id) {
-
-  client.calls
-    .create({
-      url: `https://agile-wildwood-70962.herokuapp.com/api/twiml/${id}`,
-      to: `+1${phone}`,
-      from: `+1${twilioPhone}`
-    })
-    .then(call => console.log(call.sid));
-}
